6d019c1441d3faeee66f7ea978d01e4944755dd3
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-09-04 23:44:27 +0200

    add coloring to git

--- setups.bash
+++ setups.bash
@@ -6,3 +6,7 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 echo "Adding hosts.txt to /etc/hosts"
 sudo sed -i '/127.0.1.1/ r hosts.txt' /etc/hosts
 
+echo "Add fancy coloring to git"
+echo "force_color_prompt=yes" >> ~/.bashrc
+cat gitconfig.txt >> ~/.gitconfig
+

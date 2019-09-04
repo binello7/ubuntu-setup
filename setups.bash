64e0811324c47a158f3b665cf414e46413a1d46d
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-09-04 17:02:59 +0200

    add hosts.txt to /etc/hosts

--- setups.bash
+++ setups.bash
@@ -3,3 +3,6 @@
 echo "Enabling minimize on click..."
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
+echo "Adding hosts.txt to /etc/hosts"
+sudo sed -i '/127.0.1.1/ r hosts.txt' /etc/hosts
+

50dc58aed5e2d94fc9e99328f248a74ea72aa3d5
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-09-05 14:51:21 +0200

    add polybox-client

--- apt-install.bash
+++ apt-install.bash
@@ -12,7 +12,19 @@ apt_pkgs=(
   vpnc
   network-manager-vpnc
   network-manager-vpnc-gnome
+  polybox-client
 )
+
+echo "Add new repositories to sources.list..."
+echo 'deb http://polybox.ethz.ch/repo/Ubuntu_18.04/ /' | sudo tee -a /etc/apt/sources.list.d/polybox-client.list
+
+echo "Add repositories keys..."
+wget http://polybox.ethz.ch/repo/Ubuntu_18.04/Release.key
+sudo apt-key add - < Release.key
+rm Release.key
+
+echo "Update apt-index..."
+sudo apt update
 
 for ((i=0; i<${#apt_pkgs[@]}; i++))
 do

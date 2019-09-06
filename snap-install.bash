4d9f8c2014c3ca301240b449906c5b90b43dc219
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-09-06 18:01:13 +0200

    add file

---
+++ snap-install.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+snap_pkgs=(
+  'atom --classic'
+)
+
+for ((i=0; i<${#snap_pkgs[@]}; i++))
+do
+  sudo snap install ${snap_pkgs[$i]}
+done
+
+echo -e "\nInstall of snap-packages finished"

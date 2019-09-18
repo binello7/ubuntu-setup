ec27b7c5fed3bf8728a50f7c0a98a10f9fcbaa7a
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-09-18 16:49:36 +0200

    add qgis package

--- apt-install.bash
+++ apt-install.bash
@@ -16,15 +16,19 @@ apt_pkgs=(
   libimage-exiftool-perl
   wakeonlan
   openssh-server
+  qgis
+  qgis-plugin-grass
 )
 
 echo "Add new repositories to sources.list..."
 echo 'deb http://polybox.ethz.ch/repo/Ubuntu_18.04/ /' | sudo tee -a /etc/apt/sources.list.d/polybox-client.list
+echo -e '\ndeb https://qgis.org/ubuntu bionic main' | sudo tee -a /etc/apt/sources.list
 
 echo "Add repositories keys..."
 wget http://polybox.ethz.ch/repo/Ubuntu_18.04/Release.key
 sudo apt-key add - < Release.key
 rm Release.key
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 51F523511C7028C3
 
 echo "Update apt-index..."
 sudo apt update

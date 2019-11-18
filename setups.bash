a6e624062dcce0678813a4f49aa4de6487020211
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-11-18 09:19:54 +0100

    modify ip localhost

--- setups.bash
+++ setups.bash
@@ -5,9 +5,10 @@ echo "Enabling minimize on click..."
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
 echo "Adding hosts.txt to /etc/hosts"
-sudo sed -i '/127.0.1.1/ r hosts.txt' /etc/hosts
+sudo sed -i '/127.0.0.1/ r hosts.txt' /etc/hosts
 
 echo "Add fancy coloring to git"
+echo -e "\n# Added by script 'setup.bash'" >> ~/.bashrc
 echo "force_color_prompt=yes" >> ~/.bashrc
 cat gitconfig.txt >> ~/.gitconfig
 

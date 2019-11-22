fea06fb9d83ae169698173c2a9e95c33b195ba3e
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2019-11-23 00:14:35 +0100

    add git user configuration

--- setups.bash
+++ setups.bash
@@ -7,9 +7,13 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 echo "Adding hosts.txt to /etc/hosts"
 sudo sed -i '/127.0.1.1/ r hosts.txt' /etc/hosts
 
+echo "Configuring git..."
 echo "Add fancy coloring to git"
 echo "force_color_prompt=yes" >> ~/.bashrc
 cat gitconfig.txt >> ~/.gitconfig
+echo "Configuring git user"
+git config --global user.name "Sebastiano Rusca"
+git config --global user.email sebastiano.rusca@gmail.com
 
 echo "Enabling ssh-service..."
 sudo systemctl enable ssh

6868b7205b1c914b7fd0b0520498e70e2634a6a2
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-02-10 11:30:04 +0100

    Update apt-packages

--- apt-install.bash
+++ apt-install.bash
@@ -20,6 +20,8 @@ apt_install=(
   qgis
   qgis-plugin-grass
   rar
+  r-base
+  shutter
   terminator
   unrar
   vinagre
@@ -31,12 +33,15 @@ apt_install=(
 echo "Add new repositories to sources.list..."
 echo 'deb http://polybox.ethz.ch/repo/Ubuntu_18.04/ /' | sudo tee -a /etc/apt/sources.list.d/polybox-client.list
 echo -e '\ndeb https://qgis.org/ubuntu bionic main' | sudo tee -a /etc/apt/sources.list
+sudo add-apt-repository ppa:linuxuprising/shutter
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
 
 echo "Add repositories keys..."
 wget http://polybox.ethz.ch/repo/Ubuntu_18.04/Release.key
 sudo apt-key add - < Release.key
 rm Release.key
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 51F523511C7028C3
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 
 echo "Update apt-index..."
 sudo apt update

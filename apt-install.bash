ce24a544e547ef30384f7ab00ae648731c192dfa
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2020-09-23 15:16:31 +0200

    Replace polybox with owncloud

--- apt-install.bash
+++ apt-install.bash
@@ -14,8 +14,8 @@ apt_install=(
   network-manager-vpnc-gnome
   openssh-client
   openssh-server
+  owncloud-client
   pdfshuffler
-  polybox-client
   python3-venv
   qgis
   qgis-plugin-grass
@@ -33,15 +33,13 @@ apt_install=(
 )
 
 echo "Add new repositories to sources.list..."
-echo 'deb http://polybox.ethz.ch/repo/Ubuntu_18.04/ /' | sudo tee -a /etc/apt/sources.list.d/polybox-client.list
+echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_18.04/ /' | sudo tee -a /etc/apt/sources.list.d/isv:ownCloud:desktop.list
 echo -e '\ndeb https://qgis.org/ubuntu bionic main' | sudo tee -a /etc/apt/sources.list
 sudo add-apt-repository ppa:linuxuprising/shutter
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
 
 echo "Add repositories keys..."
-wget http://polybox.ethz.ch/repo/Ubuntu_18.04/Release.key
-sudo apt-key add - < Release.key
-rm Release.key
+curl -fsSL https://download.opensuse.org/repositories/isv:ownCloud:desktop/Ubuntu_18.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/isv:ownCloud:desktop.gpg > /dev/null
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 51F523511C7028C3
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 

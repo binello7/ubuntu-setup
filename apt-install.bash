85c541dd1c47370eb0055a494ba813ff1e74269a
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2021-05-15 16:27:58 +0200

    Remove installing polybox

--- apt-install.bash
+++ apt-install.bash
@@ -18,7 +18,6 @@ apt_pkgs=(
   net-tools
   openssh-client
   openssh-server
-  polybox-client
   python3-venv
   qgis
   qgis-plugin-grass
@@ -35,15 +34,11 @@ apt_pkgs=(
 )
 
 echo "Add new repositories to sources.list..."
-echo 'deb http://polybox.ethz.ch/repo/Ubuntu_18.04/ /' | sudo tee -a /etc/apt/sources.list.d/polybox-client.list
 echo -e '\ndeb https://qgis.org/ubuntu bionic main' | sudo tee -a /etc/apt/sources.list
 sudo add-apt-repository ppa:linuxuprising/shutter
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
 
 echo "Add repositories keys..."
-wget http://polybox.ethz.ch/repo/Ubuntu_18.04/Release.key
-sudo apt-key add - < Release.key
-rm Release.key
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 51F523511C7028C3
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 
